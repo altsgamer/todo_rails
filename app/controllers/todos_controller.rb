ec0e070b8a6e24c552c18d1b2980799273c21547
Ivan Moiseev <im4adm@gmail.com>
2017-06-15 05:24:18 +0300

    Work with REST in controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,27 +1,30 @@
 class TodosController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  before_action :num_record, only: [:show, :update, :destroy]
+
+  def num_record
+    @num = Todo.primary_key.to_i
+    @res = Todo.find(params[:id])
+  end
 
   def index
-    # begin
-    # render json: [
-    #   { name: 'Todo 0', done: false },
-    #   { name: 'Todo 2', done: true },
-    #   { name: 'Todo 3', done: true },
-    #   { name: 'Todo 4', done: false }
-    # ]
-    # end
     render json: Todo.all
   end
 
   def show
+    render json: @res
   end
 
   def create
+    render json: [ { name: 'Todo 123', done: false } ]
   end
 
   def updated
+    render json: Todo.last
   end
 
   def destroy
+    render json: Todo.first
   end
 
 end
